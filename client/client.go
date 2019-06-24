5d8ccd05dae1664ff452b8b8832cc7971b524ea2
ServerSong <849867185@qq.com>
2019-06-24 19:39:29 +0800

    [modify] 添加consul重连

--- client/client.go
+++ client/client.go
@@ -1,33 +1,83 @@
 package client
 
 import (
+	"errors"
 	"fmt"
+	"log"
+	"time"
 
+	"github.com/youpenglai/mfwgo/registry"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/connectivity"
 )
 
+var (
+	grpcStateCheckTime = 10 * time.Second
+)
 
 type GRPCClient struct {
-	cc          *grpc.ClientConn
+	cc *grpc.ClientConn
+	ch chan bool
 }
 
 func getServiceUrl(serviceName string) string {
 	return fmt.Sprintf("%s:///%s", mfwScheme, serviceName)
 }
 
+func (this *GRPCClient) Alter() <-chan bool {
+	return this.ch
+}
+
 func NewGRPCConn(serviceName string) (*GRPCClient, error) {
-	conn, err := grpc.Dial(
-		getServiceUrl(serviceName),
-		grpc.WithInsecure(),
-		grpc.WithBalancerName("round_robin"), // 使用轮询调度
-	)
+	cc, err := newConn(serviceName)
 	if err != nil {
 		return nil, err
 	}
 
-	return &GRPCClient{
-		cc:          conn,
-	}, nil
+	client := GRPCClient{
+		cc: cc,
+		ch: make(chan bool, 1),
+	}
+
+	go func() {
+		ticker := time.NewTicker(grpcStateCheckTime)
+		defer ticker.Stop()
+
+		for ; ; <-ticker.C {
+			switch client.cc.GetState() {
+			case connectivity.Idle, connectivity.TransientFailure:
+				cc, err := reConnect(serviceName)
+				if err != nil {
+					log.Printf("err: %v", err.Error())
+					continue
+				}
+				client.cc = cc
+				client.ch <- true
+			}
+		}
+	}()
+
+	return &client, nil
+}
+
+func reConnect(serviceName string) (*grpc.ClientConn, error) {
+	services, err := registry.NewConsulService().GetServices(serviceName)
+	if err != nil {
+		return nil, err
+	}
+	if len(services) == 0 {
+		return nil, errors.New("Not found service: " + serviceName)
+	}
+
+	return newConn(serviceName)
+}
+
+func newConn(serviceName string) (*grpc.ClientConn, error) {
+	return grpc.Dial(
+		getServiceUrl(serviceName),
+		grpc.WithInsecure(),
+		grpc.WithBalancerName("round_robin"), // 使用轮询调度
+	)
 }
 
 func (c *GRPCClient) GetConn() *grpc.ClientConn {
